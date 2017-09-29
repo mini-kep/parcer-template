eecbad61aad470a014d4abedd64c96b3c26e4396
JaroVojtek <jarusvojtek@gmail.com>
2017-09-29 19:17:29 +0200

    tests_brent.py updated

--- parsers/getter/tests/test_brent.py
+++ parsers/getter/tests/test_brent.py
@@ -6,7 +6,7 @@ def fake_fetch(url):
     return """{"series":[{"data":[["20170925",59.42]]}]}"""
 
 def test_yield_brent_dict():
-    gen = parsers.getter.brent.yield_brent_dicts(download_func=fake_fetch)
+    gen = brent.yield_brent_dicts(download_func=fake_fetch)
     d = next(gen)
     assert d['date'] == '2017-09-25'
     assert d['value'] == Decimal('59.42')
