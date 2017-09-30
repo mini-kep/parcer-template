65969dee2c477ad58ec1eb0a133b7974729e76dc
muroslav2909 <myroslav@preply.com>
2017-09-30 14:07:48 +0300

    some notes

--- parsers/getter/tests/fixtures.py
+++ parsers/getter/tests/fixtures.py
@@ -2,7 +2,7 @@ import pytest
 
 @pytest.fixture
 def foo_obj(**kwargs):
-    foo_obj = type('', (object,), {"foo": 1})()
+    foo_obj = type('', (object,), {})()
     for name, value in kwargs.items():
         setattr(foo_obj, name, value)
     return foo_obj
