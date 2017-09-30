84d3399085aeb43a90f0c6f3a4bc3495d952b7ac
muroslav2909 <myroslav@preply.com>
2017-09-30 12:46:12 +0300

    new_tests_30_09

--- parsers/getter/tests/test_brent.py
+++ parsers/getter/tests/test_brent.py
@@ -1,8 +1,13 @@
-import pytest
 from parsers.getter import brent
 from decimal import Decimal
+import mock
 
-def fake_fetch(url):
+# I am not sure that's is a good way create fake method outsidee the test.
+# Thats why we use mocker. To patch everything that we want make fake call
+from parsers.getter.brent import parse_response
+
+
+def fake_fetch():
     return """{"series":[{"data":[["20170925",59.42]]}]}"""
 
 def test_yield_brent_dict():
@@ -10,3 +15,22 @@ def test_yield_brent_dict():
     d = next(gen)
     assert d['date'] == '2017-09-25'
     assert d['value'] == Decimal('59.42')
+
+# I want propose this way
+def test_yield_brent_dict_2():
+    fake_fetch = mock.MagicMock(return_value="""{"series":[{"data":[["20170925",59.42]]}]}""")
+    gen = brent.yield_brent_dicts(download_func=fake_fetch)
+    d = next(gen)
+    fake_fetch.assert_called_once()
+    assert d['date'] == '2017-09-25'
+    assert d['value'] == Decimal('59.42')
+
+# If we chhose way 1
+def test_parse_response_1():
+    text = fake_fetch()
+    assert parse_response(text) == [['20170925', 59.42]]
+
+# If we chhose way 2
+def test_parse_response_2():
+    text = """{"series":[{"data":[["20170925",59.42]]}]}"""
+    assert parse_response(text) == [['20170925', 59.42]]
