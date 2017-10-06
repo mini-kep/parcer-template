4e520c16943ebc6e6e4b340f84c536639b8b9b22
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-10-06 14:41:27 +0300

    Update test_brent.py

--- parsers/getter/tests/test_brent.py
+++ parsers/getter/tests/test_brent.py
@@ -1,7 +1,6 @@
 import pytest
 from decimal import Decimal
 
-# EP (delete): note cleaner imports, had duplicates
 from parsers.getter.brent import (make_url,
                                   parse_response,
                                   yield_brent_dicts)
@@ -11,18 +10,7 @@ from parsers.getter.brent import (make_url,
 def fake_fetch(url=None):
     return """{"series":[{"data":[["20170925",59.42]]}]}"""
 
-
-# EP (delete): note tests are in same order as they appear in brent.py, it
-# easier reading
-
-
-# NOTE (EP): earlier we had a parametrisation, close to this:
-# <https://github.com/requests/requests/blob/master/tests/test_requests.py#L511-L522>
-# I asked to write longname methods to descibe the tests
-# current rule is to keep parametrisation for running on different sets for data,
-# ususally this is one paramater.
-
-
+  
 def test_make_url():
     key = "NO_KEY"
     url = make_url(access_key=key)
@@ -31,7 +19,6 @@ def test_make_url():
 
 
 def test_parse_response(fake_fetch):
-    #text = fake_fetch()
     assert parse_response(fake_fetch) == [['20170925', 59.42]]
 
 
@@ -42,20 +29,6 @@ def test_yield_brent_dict():
     assert d['value'] == Decimal('59.42')
     assert d['freq'] == 'd'
     assert d['name'] == 'BRENT'
-
-
-# TODO: must test this with <from mock import patch> + decorator
-
-#from mock import patch
-#
-# class MockingTestTestCase(unittest.TestCase):
-#
-#@patch('app.mocking.get_user_name')
-# def test_mock_stubs(self, test_patch):
-#test_patch.return_value = 'Mocked This Silly'
-#ret = test_method()
-#self.assertEqual(ret, 'Mocked This Silly')
-
 
 if __name__ == "__main__":
     pytest.main([__file__])
