f2aba97e9e22b2afe234af5b23d2fa76b8ed0ede
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-10-19 17:31:19 +0300

    Update test_uploader.py

--- parsers/tests/test_uploader.py
+++ parsers/tests/test_uploader.py
@@ -7,23 +7,29 @@ from parsers.uploader import convert_decimal_to_float, to_json
 
 class Test_convert_decimal_to_float():
     def test_function_convert_decimal_to_float_return_float(self):
+        #TODO test on something bad-loooking like 1.0999999999
         decimal_number = Decimal(1)
         assert convert_decimal_to_float(decimal_number) == float(decimal_number)
 
 
 class Test_convert_gen_object_to_json():
     def test_function_convert_decimal_to_float_return_float(self):
-        #TODO add gen and expected result for comparision
-        gen = ''
-        expected_result = ''
+        #FIXME refine test to close to actual data
+        gen_sample = 
+            yield Decimal(1.01)        
+        assert to_json(gen=iter([Decimal(1.01)])) == '[1.01]'
         """
         result = json.dumps(list(gen), default=convert_decimal_to_float)
         assert result ==  expected_result
         """
-
-
+#
+#str(Decimal(1.01))
+#Out[22]: '1.0100000000000000088817841970012523233890533447265625'
+        
+        
 class Test_upload_to_database():
     def test_function_upload_to_database_returns_code_200(self):
+        #TODO write test here
         pass
 
 
